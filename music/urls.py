55418e114b77365f1aad17f2c9c0793c0b9bc9bc
trinadhkoya <trinadhkoya9@gmail.com>
2016-07-07 23:54:18 +0530

    Intro to Django

--- music/urls.py
+++ music/urls.py
@@ -3,6 +3,13 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
+
+    # /music/
     url(r'^$', views.music, name='music'),
+
+    # /music/71/
+    #  ^- represent begining and $ -represent ending
+
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.http import HttpResponse
+from django.template import  loader
+from .models import Album
 
 '''
 .means current directiory
@@ -7,4 +9,20 @@ from django.http import HttpResponse
 
 
 def music(request):
-    return HttpResponse("<html><body>Hi ,Welcome Django</body></html>")
+    all_albums = Album.objects.all();
+    music_template=loader.get_template('music.html')
+    context={
+        'all_albums':all_albums
+    }
+    #
+    # html = ''
+    #
+    # for album in all_albums:
+    #     url = '/music/' + str(album.id) + '/'
+    #     html += '<a href=' + url + '>' + album.album_title + '</a><br>'
+
+    return HttpResponse(music_template.render(context,request))
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id is " + str(album_id) + "</h2> ")
